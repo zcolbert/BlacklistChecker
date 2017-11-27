825e30274845c0dd78ad47337543d7a0b540bfd8
ZAC COLBERT <zcolbert1993@gmail.com>
2017-11-26 20:03:57 -0800

    - Refactored into classes - Added SSLMailer to send the report email

---
+++ mailer.py
@@ -0,0 +1,86 @@
+#!python3
+
+import datetime
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+class SSLMailer:
+
+    def __init__(self, server, port, username, password, message_template):
+        self.message_template = message_template
+        self.server = smtplib.SMTP_SSL(server, port)
+        self.server.login(username, password)
+        self.msg = MIMEMultipart()
+        self.initialize_message_header()
+        self.read_template()
+
+    def initialize_message_header(self):
+        today = datetime.date.today().strftime("%m-%d-%Y")
+        self.msg["Subject"] = "Blacklist Report " + today
+        self.msg["From"] = "support@myzensend.com"
+        self.msg.preamble = "Blacklist Report " + today
+
+    def read_template(self):
+        with open(self.message_template) as template:
+            html_content = template.read()
+
+        self.msg.attach(MIMEText(html_content, "html"))
+
+    def send(self, recipient):
+        self.msg["To"] = recipient
+        text = self.msg.as_string()
+        self.server.sendmail(self.msg["From"], self.msg["To"], text)
+
+
+class MessageTemplate:
+    """Write an HTML table to the file_stream object"""
+    def __init__(self, file_stream):
+        self.file_stream = file_stream
+
+    def write_html_header(self):
+        self.file_stream.write("<!DOCTYPE html>\n\n")
+        self.file_stream.write("<html>\n")
+        self.file_stream.write("\t<head></head>\n")
+        self.file_stream.write("\t<body>\n")
+
+    def write_html_footer(self):
+        self.file_stream.write("\t</body>\n")
+        self.file_stream.write("</html>\n")
+
+    def write_table_header(self):
+        self.file_stream.write("\t\t<table border=\"1\" width=\"100%\" cellpadding=\"2\" cellspacing=\"2\">\n")
+        self.file_stream.write("\t\t\t<tbody>\n")
+        self.file_stream.write("\t\t\t\t<tr>\n")
+        self.file_stream.write("\t\t\t\t\t<td>Domain Name</td>\n")
+        self.file_stream.write("\t\t\t\t\t<td>IP Address</td>\n")
+        self.file_stream.write("\t\t\t\t\t<td>Listed On</td>\n")
+        self.file_stream.write("\t\t\t\t\t<td>List Type</td>\n")
+        self.file_stream.write("\t\t\t\t</tr>\n")
+
+    def write_table_row(self, c1, c2, c3, c4):
+        self.file_stream.write("\t\t\t\t<tr>\n")
+        self.file_stream.write("\t\t\t\t\t<td>" + c1 + "</td>\n")
+        self.file_stream.write("\t\t\t\t\t<td>" + c2 + "</td>\n")
+        self.file_stream.write("\t\t\t\t\t<td>" + c3 + "</td>\n")
+        self.file_stream.write("\t\t\t\t\t<td>" + c4 + "</td>\n")
+        self.file_stream.write("\t\t\t\t</tr>\n")
+
+    def write_table_footer(self):
+        self.file_stream.write("\t\t\t</tbody>\n")
+        self.file_stream.write("\t\t</table>\n")
+
+    def generate_template_header(self):
+        self.write_html_header()
+        self.write_table_header()
+
+    def generate_template_footer(self):
+        dt = datetime.datetime.today().strftime("%m-%d-%y @ %I:%M %p")
+        self.write_table_footer()
+        self.file_stream.write("\t\t<br><p>Report generated: " + dt + "</p>\n")
+        self.write_html_footer()
+
+def test():
+    test_mailer = SSLMailer("shinari.websitewelcome.com", 465, "support@myzensend.com", "Paso93447", "files/email_template.txt")
+    print(test_mailer.msg)
+    test_mailer.send("zac@inboxwired.com")
--- main.py
+++ main.py
@@ -1,36 +1,77 @@
 #! python3
 
 import csv
-import ipdns
-from blacklists import bls, dbls
-from domains import domains
 import datetime
+import ipdns
+from domains import domains, ListedDomain
+from mailer import SSLMailer, MessageTemplate
 
+MSG_TEMPLATE = "files/email_template.txt"
 REPORT_FILE = "blacklist_report_%s.csv" % datetime.date.today().strftime("%m-%d-%Y")
 
-bl = "zen.spamhaus.org"
-dbl = "dbl.spamhaus.org"
 
-def main():
+def lookup_domains():
+    bl = "zen.spamhaus.org"
+    dbl = "dbl.spamhaus.org"
 
+    listed_domains = []
+
+    for domain in domains:
+        print("Checking", domain, "...")
+        ip = ipdns.resolve_ip(domain)
+
+        ip_status = ipdns.bl_lookup_by_ip(ip, bl)
+        if ip_status is not None:
+            temp_domain = ListedDomain(domain, ip, bl, "IP Address")
+            listed_domains.append(temp_domain)
+
+        dns_status = ipdns.dbl_lookup(domain, dbl)
+        if dns_status is not None:
+            temp_domain = ListedDomain(domain, ip, dbl, "Domain")
+            listed_domains.append(temp_domain)
+
+    return listed_domains
+
+
+def generate_report(listed_domains):
+    # Generate the report
     with open(REPORT_FILE, 'w', newline="") as report:
         writer = csv.writer(report, dialect='excel')
         writer.writerow(["Domain Name", "IP Address", "Listed On", "List Type"])
 
-        for domain in domains:
+        for domain in listed_domains:
+            writer.writerow([domain.domain_name,
+                             domain.ip_address,
+                             domain.list_name,
+                             domain.list_type])
 
-            print("Checking", domain, "...")
-            ip = ipdns.resolve_ip(domain)
-            ip_status = ipdns.bl_lookup_by_ip(ip, bl)
-            dns_status = ipdns.dbl_lookup(domain, dbl)
 
-            if not ip_status == None:
-                print(domain, " - IP is listed", ip)
-                writer.writerow([domain, ip, bl, "IP Address"])
+def generate_email_template(listed_domains):
+    with open(MSG_TEMPLATE, 'w') as template_file:
+        msg = MessageTemplate(template_file)
 
-            if not dns_status == None:
-                print(domain, "is listed on", dbl)
-                writer.writerow([domain, ip, dbl, "Domain"])
+        msg.generate_template_header()
 
+        for domain in listed_domains:
+            msg.write_table_row(domain.domain_name,
+                                domain.ip_address,
+                                domain.list_name,
+                                domain.list_type)
+
+        msg.generate_template_footer()
+
+def send_report_email(recipient):
+    mailer = SSLMailer("shinari.websitewelcome.com", 465,
+                            "support@myzensend.com", "Paso93447",
+                            MSG_TEMPLATE)
+    mailer.send(recipient)
+
+
+def main():
+
+    # Check each of the domains against the BL and DBL
+    listed_domains = lookup_domains()  # Contains ListedDomain objects
+    generate_email_template(listed_domains)
+    #send_report_email("support@myzensend.com")
 
 main()
