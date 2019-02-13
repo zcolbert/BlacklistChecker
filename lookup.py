f03999de9898649d405a946006a6c73ca8347abb
ZAC COLBERT <zcolbert1993@gmail.com>
2019-02-12 23:54:15 -0800

    Updated report

--- lookup.py
+++ lookup.py
@@ -25,9 +25,13 @@ def load_domains_from_csv(filename, status='', delimiter=',', domain_field='Doma
 def print_blacklist_report(status):
     print('=' * 48)
     domain = status.domain
-    print('{domain} ({ip})'.format(
-        domain=domain.name, ip=domain.ipv4_address))
+    if domain.is_active():
+        ip = domain.ipv4_address
+    else:
+        ip = "Offline"
+    print('{domain} ({ip})'.format(domain=domain.name, ip=ip))
     print('{:-<48}'.format(''))
+
     for b in status.blacklists:
         print('{:<12} @ {:<32}'.format(
             b.query_type, b.alias
@@ -53,20 +57,29 @@ def load_blacklists_from_csv(filename):
     return lists
 
 
-def create_csv_report(listed_domains, account_name=''):
-    fieldnames = ['Domain', 'IP', 'Domain Listed', 'IP Listed']
+def create_csv_report(results, account_name=''):
+    fieldnames = ['Domain', 'IP Address', 'Domain Status', 'IP Status']
     filename = '_'.join((account_name, 'Listed_Report.csv')).strip('_')
+
+    print('Creating', filename, '... ', end='')
+
     with open(filename, 'w', newline='') as srcfile:
         writer = csv.DictWriter(srcfile, fieldnames=fieldnames)
         writer.writeheader()
 
-        for domain in listed_domains:
+        for r in results:
             row = OrderedDict()
-            row['Domain'] = domain.name
-            row['IP'] = domain.get_ipv4()
-            row['Domain Listed'] = domain.domain_is_listed()
-            row['IP Listed'] = domain.ip_is_listed()
+            row['Domain'] = r.domain.name
+
+            if r.domain.is_active():
+                row['IP Address'] = r.domain.ipv4_address
+            else:
+                row['IP Address'] = 'Offline'
+
+            row['Domain Status'] = ', '.join([bl.alias for bl in r.domain_listings])
+            row['IP Status'] = ', '.join([bl.alias for bl in r.ip_listings])
             writer.writerow(row)
+    print('Done.')
 
 
 def lookup_domain(domain, checker):
@@ -85,11 +98,12 @@ def lookup_domains():
     checker = BlacklistChecker(blacklists)
     domains = load_domains_from_csv('C:/Users/Zachary/Desktop/servers.csv', delimiter=',')
 
+    results = []
     for domain in domains:
-        result = lookup_domain(domain, checker)
-        print_blacklist_report(result)
+        print('Checking', domain, '...')
+        results.append(lookup_domain(domain, checker))
 
-    #create_csv_report(checker.get_listed_domains())
+    create_csv_report(results)
 
 
 def get_args():
