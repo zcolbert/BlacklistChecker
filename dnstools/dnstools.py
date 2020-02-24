10bdf541ab2909583e11a0e76f80afbbd478b8de
ZAC COLBERT <zcolbert1993@gmail.com>
2020-02-24 14:33:46 -0800

    Remove DnsResolver class and replace with module level functions

---
+++ dnstools/dnstools.py
@@ -0,0 +1,36 @@
+from typing import List
+
+import dns.resolver
+import socket
+
+
+def query(domain_name: str, record_type: str) -> List[str]:
+    try:
+        resolver = dns.resolver.Resolver()
+        answers = resolver.query(domain_name, record_type)
+        return [rdata.to_text() for rdata in answers]
+    except dns.rdatatype.UnknownRdatatype:
+        msg = "Invalid DNS record type: {}"
+        raise ValueError(msg.format(record_type))
+    except (dns.resolver.NXDOMAIN,
+            dns.resolver.NoAnswer,
+            dns.resolver.NoNameservers,
+            dns.resolver.Timeout) as err:
+        # lookup returned no result
+        return list()
+
+
+def query_ipv4_from_host(domain: str) -> List[str]:
+    return query(domain, record_type='a')
+
+
+def host_is_active(hostname: str) -> bool:
+    return query_ipv4_from_host(hostname) != list()
+
+
+def ip_is_active(ip_address: str) -> bool:
+    try:
+        socket.gethostbyaddr(ip_address)
+        return True
+    except socket.herror as err:
+        return False
--- dnstools/tests/resolver_tests.py
+++ dnstools/tests/resolver_tests.py
@@ -1,42 +1,39 @@
 import unittest
-from dnstools.resolver import DnsResolver
+import dnstools
 
 
 class DnsResolverTest(unittest.TestCase):
 
-    def setUp(self) -> None:
-        self.resolver = DnsResolver()
-
     def test_query_empty_domain_returns_empty_list(self):
         domain = ''
-        answer = self.resolver.query_ipv4_from_host(domain)
+        answer = dnstools.query_ipv4_from_host(domain)
         self.assertListEqual(answer, [])
 
     def test_query_invalid_domain_returns_empty_list(self):
         domain = 'notarealdomain.somefaketld'
-        answer = self.resolver.query_ipv4_from_host(domain)
+        answer = dnstools.query_ipv4_from_host(domain)
         self.assertListEqual(answer, [])
 
     def test_query_live_domain_returns_list_of_ip_addresses(self):
         domain = 'google.com'
-        answer = self.resolver.query_ipv4_from_host(domain)
+        answer = dnstools.query_ipv4_from_host(domain)
         self.assertNotEqual(answer, [])
 
     def test_host_is_active_returns_true_with_active_domain(self):
         domain = 'google.com'
-        self.assertTrue(self.resolver.host_is_active(domain))
+        self.assertTrue(dnstools.host_is_active(domain))
 
     def test_host_is_active_returns_false_with_inactive_domain(self):
         domain = 'notarealdomain.somefaketld'
-        self.assertFalse(self.resolver.host_is_active(domain))
+        self.assertFalse(dnstools.host_is_active(domain))
 
     def test_ip_is_active_returns_true_with_live_ip(self):
         ip = '8.8.8.8'  # google DNS server
-        self.assertTrue(self.resolver.ip_is_active(ip))
+        self.assertTrue(dnstools.ip_is_active(ip))
 
     def test_ip_is_active_returns_false_with_fake_ip(self):
         ip = '203.0.113.0'  # invalid IP - RFC 5737
-        self.assertFalse(self.resolver.ip_is_active(ip))
+        self.assertFalse(dnstools.ip_is_active(ip))
 
 
 if __name__ == '__main__':
