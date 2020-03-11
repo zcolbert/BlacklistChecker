bf34dabbb8100f5a27800322fbc2b921008d587d
ZAC COLBERT <zcolbert1993@gmail.com>
2020-03-11 14:33:32 -0700

    Move dns exception classes to exception module

--- dnstools/tests/dnstools_tests.py
+++ dnstools/tests/dnstools_tests.py
@@ -6,12 +6,12 @@ class DnsResolverTest(unittest.TestCase):
 
     def test_query_empty_domain_raises_EmptyHostError(self):
         domain = ''
-        with self.assertRaises(dnstools.dnstools.EmptyHostError):
+        with self.assertRaises(dnstools.exception.EmptyHostError):
             dnstools.query_ipv4_from_host(domain)
 
     def test_query_invalid_domain_raises_HostError(self):
         domain = 'notarealdomain.somefaketld'
-        with self.assertRaises(dnstools.dnstools.HostError):
+        with self.assertRaises(dnstools.exception.HostError):
             dnstools.query_ipv4_from_host(domain)
 
     def test_query_live_domain_returns_list_of_ip_addresses(self):
@@ -27,11 +27,11 @@ class DnsResolverTest(unittest.TestCase):
         domain = 'notarealdomain.somefaketld'
         self.assertFalse(dnstools.host_is_active(domain))
 
-    def test_ip_is_active_returns_true_with_live_ip(self):
+    def test_ip_is_active_returns_true_from_live_ip(self):
         ip = '8.8.8.8'  # google DNS server
         self.assertTrue(dnstools.ip_is_active(ip))
 
-    def test_ip_is_active_returns_false_with_fake_ip(self):
+    def test_ip_is_active_returns_false_from_fake_ip(self):
         ip = '203.0.113.0'  # invalid IP - RFC 5737
         self.assertFalse(dnstools.ip_is_active(ip))
 
